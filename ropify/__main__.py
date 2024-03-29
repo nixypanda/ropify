4f924ceeeae6850f1bda33171b36d2e7a07327ac
Sherub Thakur <sherub.thakur@gmail.com>
2024-03-29 14:58:45 +0530

    refactor: destination is not optional

--- ropify/__main__.py
+++ ropify/__main__.py
@@ -1,16 +1,14 @@
-import os
-from contextlib import redirect_stdout
 from pathlib import Path
 from typing import Callable
 
 import click
 from click.decorators import FC
 from rope.base.fscommands import FileSystemCommands
-from rope.base.libutils import is_python_file, path_to_resource
+from rope.base.libutils import path_to_resource
 from rope.base.project import Project
 from rope.base.pynames import ImportedModule
-from rope.contrib.autoimport.sqlite import AutoImport
-from rope.refactor.move import create_move
+from rope.base.resources import File, Folder
+from rope.refactor.move import MoveModule, create_move
 
 
 @click.group()
@@ -44,85 +42,42 @@ def _create_rope_project(project: Path, ropefolder: str | None) -> Project:
 
 
 @cli.command()
-@click.argument(
-    "resource",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path),
-)
-@click.option(
-    "--destination",
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-    help=(
-        "The path to the package where to move the module. "
-        "If not provided, the user will be prompted for it."
-    ),
-)
+@click.argument("resource", type=click.Path(exists=True, dir_okay=False, path_type=Path))
+@click.argument("destination", type=click.Path(exists=True, file_okay=False, path_type=Path))
 @project_option()
 @ropefolder_option()
-def move_module(
-    resource: Path,
-    destination: Path | None,
-    project: Path,
-    ropefolder: str | None,
-) -> None:
+def move_module(resource: Path, destination: Path, project: Path, ropefolder: str | None) -> None:
     """
     Move a module to another package.
 
     \b
     RESOURCE: The path to the module file.
+    DESTINATION: The path to the destination folder.
     """
 
     rope_project = _create_rope_project(project, ropefolder)
+    m_source = path_to_resource(rope_project, resource)
+    m_dest = path_to_resource(rope_project, destination)
+    assert isinstance(m_source, File) and isinstance(m_dest, Folder)
 
-    rope_resource = path_to_resource(rope_project, resource)
-    move = create_move(rope_project, rope_resource)
-    module_name = move.old_name
-    click.echo(f"Moving definition of `{module_name}`")
+    move = create_move(rope_project, m_source)
+    assert isinstance(move, MoveModule)
+    click.echo(f"Moving definition of `{move.old_name}`")
+    click.echo(f"Definition is currently at: {m_source.path}")
 
-    module_source = rope_resource
-    click.echo(f"Definition is currently at: {module_source.path}")
-
-    if destination is None:
-        destination = click.prompt(
-            "Enter the destination for the module",
-            type=click.Path(exists=False, path_type=Path),
-        )
-
-    module_destination = path_to_resource(rope_project, destination)
-
-    if not module_destination.is_folder():
-        click.echo("The destination must be a python package")
-        exit(1)
-
-    changes = move.get_changes(module_destination)
+    changes = move.get_changes(m_dest)
     rope_project.do(changes)
-    click.echo(f"Module `{module_name}` moved to: {module_destination.path}")
+    click.echo(f"Module `{m_source.path}` moved to: {m_dest.path}")
 
 
 @cli.command()
-@click.argument(
-    "resource",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path),
-)
-@click.argument(
-    "offset",
-    type=click.IntRange(min=0),
-)
-@click.option(
-    "--destination",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path),
-    help=(
-        "The path where to move the definition to. "
-        "If not provided, the user will be prompted for it."
-    ),
-)
+@click.argument("resource", type=click.Path(exists=True, dir_okay=False, path_type=Path))
+@click.argument("offset", type=click.IntRange(min=0))
+@click.argument("destination", type=click.Path(exists=True, dir_okay=False, path_type=Path))
 @project_option()
 @ropefolder_option()
-def move_symbol(
-    resource: Path,
-    offset: int,
-    destination: Path | None,
-    project: Path,
-    ropefolder: str | None,
+def move_symbol_offset(
+    resource: Path, offset: int, destination: Path, project: Path, ropefolder: str | None
 ) -> None:
     """
     Move the definition of a global symbol to another file.
@@ -131,11 +86,19 @@ def move_symbol(
     RESOURCE: The path to the file containing the symbol to move.
     OFFSET: The byte offset of the symbol within the file.
     """
+    move_with_offset(resource, destination, offset, project, ropefolder)
 
+
+def move_with_offset(
+    source: Path, destination: Path, offset: int, project: Path, ropefolder: str | None
+) -> None:
     rope_project = _create_rope_project(project, ropefolder)
 
-    rope_resource = path_to_resource(rope_project, resource)
-    move = create_move(rope_project, rope_resource, offset)
+    f_source = path_to_resource(rope_project, source)
+    f_dest = path_to_resource(rope_project, destination)
+    assert isinstance(f_source, File) and isinstance(f_dest, File)
+    move = create_move(rope_project, f_source, offset)
+
     symbol_name = move.old_name
     click.echo(f"Moving definition of `{symbol_name}`")
 
@@ -146,53 +109,9 @@ def move_symbol(
     symbol_def_source = move.old_pyname.get_definition_location()[0].get_resource()
     click.echo(f"Definition is currently at: {symbol_def_source.path}")
 
-    if destination is None:
-        destination = click.prompt(
-            "Enter the destination file for the definition",
-            type=click.Path(exists=True, dir_okay=False, path_type=Path),
-        )
-
-    symbol_def_destination = path_to_resource(rope_project, destination)
-
-    if not is_python_file(rope_project, symbol_def_destination):
-        click.echo("The destination must be a python file")
-        exit(1)
-
-    changes = move.get_changes(symbol_def_destination)
+    changes = move.get_changes(f_dest)
     rope_project.do(changes)
-    click.echo(f"Definition of `{symbol_name}` moved to: {symbol_def_destination.path}")
-
-
-@cli.command()
-@click.argument("name", type=click.STRING)
-@project_option()
-@ropefolder_option()
-def show_imports(name: str, project: Path, ropefolder: str | None) -> None:
-    """
-    Print the candidate imports for a given name to stdout.
-
-    \b
-    NAME: The name to get candidate imports for.
-    """
-
-    rope_project = _create_rope_project(project, ropefolder)
-
-    autoimport = AutoImport(rope_project, memory=False)
-
-    # Silence rope complaining about syntax errors during cache generation
-    with open(os.devnull, "w") as f, redirect_stdout(f):
-        autoimport.generate_cache()
-        autoimport.generate_modules_cache()
-
-    raw_modules: list[str] = autoimport.get_modules(name)
-    modules = [m for m in raw_modules if not m.startswith("site-packages.")]
-
-    if len(modules) == 0:
-        click.echo(f"No modules found for {name}")
-        exit(1)
-
-    for module in modules:
-        click.echo(f"from {module} import {name}")
+    click.echo(f"Definition of `{symbol_name}` moved to: {f_dest.path}")
 
 
 def main() -> None:
